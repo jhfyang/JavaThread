f98cd87fcf06f1a3586e161d26289517c9b6d7a1
SB\jyang <y4ng.jh@gmail.com>
2017-02-25 18:44:09 +0100

    Finished Ex 3

--- src/javathread/PingPong.java
+++ src/javathread/PingPong.java
@@ -1,7 +1,13 @@
+package javathread;
+
+import java.util.concurrent.locks.Lock;
+import java.util.concurrent.locks.ReentrantLock;
+
 public class PingPong {
 
     private boolean sent = false;
-
+    final Lock lock = new ReentrantLock();
+    
     private void ping() {
         sent = true;
         System.out.println("ping");
@@ -9,9 +15,12 @@ public class PingPong {
 
     private void pong() throws InterruptedException {
         while (sent == false) {
-            Thread.sleep(100);
+            //This makes the thread to sleep for 100 milliseconds
+            Thread.sleep(0);
         }
+        this.lock.lock();
         System.out.println("pong");
+        this.lock.unlock();
     }
 
     public static void main(String[] args) throws InterruptedException {
@@ -20,12 +29,16 @@ public class PingPong {
 
             @Override
             public void run() {
+                pingPong.lock.lock();
                 try {
                     Thread.sleep(2000);
+                    pingPong.ping();
                 } catch (InterruptedException e) {
                     new AssertionError(e);
+                } finally {
+                    pingPong.lock.unlock();
                 }
-                pingPong.ping();
+                
             }
 
         }).start();
