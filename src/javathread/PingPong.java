eee9c48ae3381115c2e8bdf0453b9caf2e413621
SB\jyang <y4ng.jh@gmail.com>
2017-02-27 21:16:57 +0100

    PingPong Finished

--- src/javathread/PingPong.java
+++ src/javathread/PingPong.java
@@ -1,17 +1,37 @@
+package javathread;
+
+import java.util.concurrent.locks.Condition;
+import java.util.concurrent.locks.Lock;
+import java.util.concurrent.locks.ReentrantLock;
+
 public class PingPong {
 
     private boolean sent = false;
+    final Lock lock = new ReentrantLock();
+    final Condition pingSent = lock.newCondition();
+    final Condition pongSent = lock.newCondition();
+
 
     private void ping() {
         sent = true;
         System.out.println("ping");
+        this.pingSent.signal();
     }
 
-    private void pong() throws InterruptedException {
-        while (sent == false) {
-            Thread.sleep(100);
+    private synchronized void pong() throws InterruptedException {
+        this.lock.lock();
+        try {
+            while (sent == false) {
+                //This makes the thread to sleep for 100 milliseconds
+                //Thread.sleep(0);
+                this.pingSent.await();
+            } if (sent == true) {
+                System.out.println("pong");
+                this.pongSent.signal();
+            }
+        } finally {
+            this.lock.unlock();
         }
-        System.out.println("pong");
     }
 
     public static void main(String[] args) throws InterruptedException {
@@ -20,12 +40,16 @@ public class PingPong {
 
             @Override
             public void run() {
+                pingPong.lock.lock();
                 try {
                     Thread.sleep(2000);
+                    pingPong.ping();
                 } catch (InterruptedException e) {
                     new AssertionError(e);
+                } finally {
+                    pingPong.lock.unlock();
                 }
-                pingPong.ping();
+
             }
 
         }).start();
